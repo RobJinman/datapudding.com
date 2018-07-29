d1eb6fece7ba33ad4ee7cc3842e5470bacf7ca55
Rob Jinman <jinmanr@gmail.com>
2018-07-29 15:19:04 +0100

    Replaced datapudding-server project with one created from pedestal template

--- datapudding-server/project.clj
+++ datapudding-server/project.clj
@@ -1,9 +1,19 @@
-(defproject datapudding-server "0.1.0-SNAPSHOT"
+(defproject datapudding-server "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot datapudding-server.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [io.pedestal/pedestal.service "0.5.4"]
+                 [io.pedestal/pedestal.jetty "0.5.4"]
+                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.25"]
+                 [org.slf4j/jcl-over-slf4j "1.7.25"]
+                 [org.slf4j/log4j-over-slf4j "1.7.25"]]
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "datapudding-server.server/run-dev"]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.4"]]}
+             :uberjar {:aot [datapudding-server.server]}}
+  :main ^{:skip-aot true} datapudding-server.server)
+
--- datapudding-server/test/datapudding_server/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns datapudding-server.core-test
-  (:require [clojure.test :refer :all]
-            [datapudding-server.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
